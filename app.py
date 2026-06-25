e5434111dc4f13a10718b2ca4959ca28716ec78e
yossi-lerer <yossi.lerer.2003@gmail.com>
2026-06-25 10:42:08 +0300

    create the main loop and match case inside

---
+++ app.py
@@ -0,0 +1,17 @@
+while True:
+    menu_input = int(input("enter your choose "))
+    match menu_input:
+        case 6:
+            break
+        case 1:
+            pass
+        case 2:
+            pass
+        case 3:
+            pass
+        case 4:
+            pass
+        case 5:
+            pass
+        case _:
+            pass
